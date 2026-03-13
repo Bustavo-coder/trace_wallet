9aa356c9bd3d32cd5c28dd699717ee1df66d377a
Bustavo-coder <abdbasitegberongbe@gmail.com>
2026-03-13 15:51:55 +0100

    "debugging serilizer"

--- service/deposit_service.py
+++ service/deposit_service.py
@@ -4,7 +4,8 @@ from django.db.models.fields import return_None
 
 from wallet.models import Wallet
 from wallet.service.deposit_service import deposit
-from notification.services import create_transfer_notification
+from notification.services import create_deposit_notification
+
 
 def deposit_service(sender:Wallet,amount,idempotency_key:UUID):
    tx =  deposit(sender,amount,idempotency_key)
