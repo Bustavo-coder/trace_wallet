64b8be4c429d643a28356c22d1dcadc82e987e0b
Bustavo-coder <abdbasitegberongbe@gmail.com>
2026-05-13 15:32:06 +0100

    removed secret key

--- traceWallet/settings.py
+++ traceWallet/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG','False').lower() == 'true'
 
-ALLOWED_HOSTS = [
-]
+ALLOWED_HOSTS = [os.getenv('AllOWED_HOST')]
 
 
 # Application definition
